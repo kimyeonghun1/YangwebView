71fc23c5b4d5dbc459c22e159b36f8fe1a8a7e69
김영훈 <92971200+kimyeonghun1@users.noreply.github.com>
2021-12-20 12:18:04 +0900

    이러다 다죽어
    다시 올리긩ㅇㅇㅇ

--- 2차프로젝트/yangyang/WebContent/assets/js/외부.js
+++
@@ -1,7 +0,0 @@
-// 스크립트 공간 할당 > JS 쓰는 공간 
-    document.getElementById("btn").addEventListener("click", function(){
-        // 클릭했을 때 돌아가는 로직을 작성하는 공간
-        document.getElementById("design").style.color="blue";
-        document.getElementById("design").style.backgroundColor="yellow";
-    })
-    
